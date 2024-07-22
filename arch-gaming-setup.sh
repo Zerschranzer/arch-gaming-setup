21a22198a1622e726b6efc12e6f30402afe60d59
Zerschranzer <zerschranzer@googlemail.com>
2024-07-22 23:33:25 +0200

    Update arch-gaming-setup.sh

--- arch-gaming-setup.sh
+++ arch-gaming-setup.sh
@@ -107,7 +107,7 @@ main_installation() {
     sudo pacman -S --noconfirm steam lutris wine-staging winetricks gamemode lib32-gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal \
     v4l-utils lib32-v4l-utils libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama \
     lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader \
-    lib32-vulkan-icd-loader obs-studio discord flatpak mangohud lib32-mangohud goverlay gamescope solaar bluez bluez-utils pipewire pipewire-pulse pipewire-alsa linux-headers
+    lib32-vulkan-icd-loader obs-studio discord mangohud lib32-mangohud goverlay gamescope solaar bluez bluez-utils pipewire pipewire-pulse pipewire-alsa linux-headers
 
     echo "Installing AUR packages with yay..."
     yay -S --noconfirm \
