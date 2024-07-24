476466f67428805cb76a840599b4bad61e161379
Zerschranzer <zerschranzer@googlemail.com>
2024-07-25 00:06:17 +0200

    Update arch-auto-installer.sh

--- arch-auto-installer.sh
+++ arch-auto-installer.sh
@@ -2,16 +2,32 @@
 
 # Interactive Arch Linux Installation Script
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+# Function to print colored output
+print_color() {
+    COLOR=$1
+    MESSAGE=$2
+    echo -e "${COLOR}${MESSAGE}${NC}"
+}
+
 # Function to select keyboard layout
 select_keyboard_layout() {
-    echo "Common keyboard layouts:"
-    echo "1) US English (us)"
-    echo "2) UK English (uk)"
-    echo "3) German (de)"
-    echo "4) French (fr)"
-    echo "5) Spanish (es)"
-    echo "6) Italian (it)"
-    echo "7) Other (list all)"
+    print_color "$YELLOW" "Common keyboard layouts:"
+    print_color "$MAGENTA" "1) US English (us)"
+    print_color "$MAGENTA" "2) UK English (uk)"
+    print_color "$MAGENTA" "3) German (de)"
+    print_color "$MAGENTA" "4) French (fr)"
+    print_color "$MAGENTA" "5) Spanish (es)"
+    print_color "$MAGENTA" "6) Italian (it)"
+    print_color "$MAGENTA" "7) Other (list all)"
     
     read -p "Please select a number (1-7): " layout_choice
     
@@ -23,30 +39,30 @@ select_keyboard_layout() {
         5) keyboard_layout="es" ;;
         6) keyboard_layout="it" ;;
         7)
-            echo "Available keyboard layouts:"
+            print_color "$BLUE" "Available keyboard layouts:"
             localectl list-keymaps
             read -p "Please enter the desired keyboard layout: " keyboard_layout
             ;;
         *)
-            echo "Invalid choice. Defaulting to US English."
+            print_color "$RED" "Invalid choice. Defaulting to US English."
             keyboard_layout="us"
             ;;
     esac
     
     loadkeys $keyboard_layout
-    echo "Keyboard layout set to $keyboard_layout."
+    print_color "$GREEN" "Keyboard layout set to $keyboard_layout."
 }
 
 # Function to select locale
 select_locale() {
-    echo "Common locales:"
-    echo "1) en_US.UTF-8"
-    echo "2) en_GB.UTF-8"
-    echo "3) de_DE.UTF-8"
-    echo "4) fr_FR.UTF-8"
-    echo "5) es_ES.UTF-8"
-    echo "6) it_IT.UTF-8"
-    echo "7) Other (manual input)"
+    print_color "$YELLOW" "Common locales:"
+    print_color "$MAGENTA" "1) en_US.UTF-8"
+    print_color "$MAGENTA" "2) en_GB.UTF-8"
+    print_color "$MAGENTA" "3) de_DE.UTF-8"
+    print_color "$MAGENTA" "4) fr_FR.UTF-8"
+    print_color "$MAGENTA" "5) es_ES.UTF-8"
+    print_color "$MAGENTA" "6) it_IT.UTF-8"
+    print_color "$MAGENTA" "7) Other (manual input)"
     
     read -p "Please select a number (1-7): " locale_choice
     
@@ -61,33 +77,36 @@ select_locale() {
             read -p "Please enter your desired locale (e.g., ja_JP.UTF-8): " user_locale
             ;;
         *)
-            echo "Invalid choice. Defaulting to en_US.UTF-8."
+            print_color "$RED" "Invalid choice. Defaulting to en_US.UTF-8."
             user_locale="en_US.UTF-8"
             ;;
     esac
     
-    echo "Selected locale: $user_locale"
+    print_color "$GREEN" "Selected locale: $user_locale"
 }
 
 # Function to select disk and partitioning method
 select_disk() {
-    echo "Available disks:"
+    print_color "$YELLOW" "Available disks:"
     lsblk
-    read -p "Please enter the device name of the disk to be used (e.g., sda): " disk
-    echo "You have selected /dev/$disk."
-    echo "1) Automatic partitioning (will erase all data)"
-    echo "2) Manual partitioning"
-    read -p "Choose partitioning method (1/2): " part_method
+    echo -e "${YELLOW}Please enter the device name of the disk to be used (e.g., sda):${NC} "
+    read disk
+    print_color "$BLUE" "You have selected /dev/$disk."
+    print_color "$MAGENTA" "1) Automatic partitioning (will erase all data)"
+    print_color "$MAGENTA" "2) Manual partitioning"
+    echo -e "${YELLOW}Choose partitioning method (1/2):${NC} "
+    read part_method
     
     if [[ $part_method == "1" ]]; then
-        echo "Automatic partitioning will erase all data on /dev/$disk."
+        print_color "$RED" "Automatic partitioning will erase all data on /dev/$disk."
     else
-        echo "Manual partitioning selected. You will use cfdisk to partition the disk."
+        print_color "$BLUE" "Manual partitioning selected. You will use cfdisk to partition the disk."
     fi
     
-    read -p "Continue? (y/n): " confirm
+    echo -e "${YELLOW}Continue? (y/n):${NC} "
+    read confirm
     if [[ $confirm != "y" ]]; then
-        echo "Aborted."
+        print_color "$RED" "Aborted."
         exit 1
     fi
 }
@@ -98,14 +117,15 @@ auto_partition() {
     local efi_size=512  # Size in MiB
     local swap_size=0   # Size in MiB, 0 means no swap
     
-    # Ask if user wants a swap partition
-    echo "Do you want to create a swap partition?"
-    echo "1) Yes"
-    echo "2) No"
-    read -p "Enter your choice (1/2): " swap_choice
+    print_color "$YELLOW" "Do you want to create a swap partition?"
+    print_color "$MAGENTA" "1) Yes"
+    print_color "$MAGENTA" "2) No"
+    echo -e "${YELLOW}Enter your choice (1/2):${NC} "
+    read swap_choice
     
     if [[ $swap_choice == "1" ]]; then
-        read -p "Enter the size of swap partition in MiB " swap_size
+        echo -e "${YELLOW}Enter the size of swap partition in MiB:${NC} " 
+        read swap_size
     fi
     
     # Calculate the remaining space for root partition (in sectors)
@@ -139,12 +159,12 @@ auto_partition() {
         root_partition="${disk}2"
     fi
 
-    echo "Disk $disk has been automatically partitioned:"
-    echo "EFI System Partition: /dev/${efi_partition} (${efi_size} MiB)"
+    print_color "$GREEN" "Disk $disk has been automatically partitioned:"
+    print_color "$BLUE" "EFI System Partition: /dev/${efi_partition} (${efi_size} MiB)"
     if [[ $swap_size -gt 0 ]]; then
-        echo "Swap Partition: /dev/${swap_partition} (${swap_size} MiB)"
+        print_color "$BLUE" "Swap Partition: /dev/${swap_partition} (${swap_size} MiB)"
     fi
-    echo "Root Partition: /dev/${root_partition} (remaining space)"
+    print_color "$BLUE" "Root Partition: /dev/${root_partition} (remaining space)"
 
     # Format partitions
     mkfs.fat -F32 /dev/${efi_partition}
@@ -162,13 +182,14 @@ auto_partition() {
 
 # Function to select filesystem
 select_filesystem() {
-    echo "Select filesystem for the root partition:"
-    echo "1) ext4 (default)"
-    echo "2) btrfs"
-    echo "3) xfs"
-    echo "4) f2fs"
+    print_color "$YELLOW" "Select filesystem for the root partition:"
+    print_color "$MAGENTA" "1) ext4 (default)"
+    print_color "$MAGENTA" "2) btrfs"
+    print_color "$MAGENTA" "3) xfs"
+    print_color "$MAGENTA" "4) f2fs"
     
-    read -p "Please select a number (1-4): " fs_choice
+    echo -e "${YELLOW}Please select a number (1-4):${NC} " 
+    read fs_choice
     
     case $fs_choice in
         1) filesystem="ext4" ;;
@@ -176,18 +197,19 @@ select_filesystem() {
         3) filesystem="xfs" ;;
         4) filesystem="f2fs" ;;
         *)
-            echo "Invalid choice. Defaulting to ext4."
+            print_color "$RED" "Invalid choice. Defaulting to ext4."
             filesystem="ext4"
             ;;
     esac
     
-    echo "Selected filesystem: $filesystem"
+    print_color "$GREEN" "Selected filesystem: $filesystem"
 }
 
 # Main script
-echo "Welcome to the interactive Arch Linux installation script!"
+print_color "$CYAN" "Welcome to the interactive Arch Linux installation script!"
 
 # Update system clock
+print_color "$BLUE" "Updating system clock..."
 timedatectl set-ntp true
 
 # Select keyboard layout
@@ -203,21 +225,22 @@ select_disk
 if [[ $part_method == "1" ]]; then
     auto_partition $disk
 else
-    echo "We will now proceed with manual partitioning."
-    echo "It is recommended to create at least two partitions:"
-    echo "1. An EFI System Partition (at least 500M)"
-    echo "2. A Root Partition (rest of the disk)"
+    print_color "$YELLOW" "We will now proceed with manual partitioning."
+    print_color "$BLUE" "It is recommended to create at least two partitions:"
+    print_color "$BLUE" "1. An EFI System Partition (at least 500M)"
+    print_color "$BLUE" "2. A Root Partition (rest of the disk)"
     read -p "Press Enter to continue..."
     cfdisk /dev/$disk
 
     # Format partitions
-    echo "Please enter the partition numbers:"
+    print_color "$YELLOW" "Please enter the partition numbers:"
     read -p "EFI System Partition (e.g., 1 for ${disk}1): " efi_partition
     read -p "Root Partition (e.g., 2 for ${disk}2): " root_partition
 
     # Select filesystem for root partition
     select_filesystem
 
+    print_color "$BLUE" "Formatting partitions..."
     mkfs.fat -F32 /dev/${disk}${efi_partition}
 
     # Format root partition with selected filesystem
@@ -237,34 +260,40 @@ else
     esac
 
     # Mount partitions
+    print_color "$BLUE" "Mounting partitions..."
     mount /dev/${disk}${root_partition} /mnt
     mkdir /mnt/boot
     mount /dev/${disk}${efi_partition} /mnt/boot
 fi
 
 # Install base package and Linux kernel
+print_color "$CYAN" "Installing base system..."
 pacstrap /mnt base linux linux-firmware
 
 # If btrfs was selected, install btrfs-progs
 if [ "$filesystem" == "btrfs" ]; then
+    print_color "$BLUE" "Installing btrfs-progs..."
     pacstrap /mnt btrfs-progs
 fi
 
 # Generate fstab
+print_color "$BLUE" "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Gather user input before chroot
-read -p "Please enter the desired hostname: " hostname
-read -p "Please enter your desired username: " username
-read -p "Please enter your desired time zone (e.g., Europe/London): " timezone
+print_color "$YELLOW" "Please enter the following information:"
+read -p "Desired hostname: " hostname
+read -p "Desired username: " username
+read -p "Desired time zone (e.g., Europe/London): " timezone
 
 # Set passwords
+print_color "$YELLOW" "Setting up passwords:"
 read -s -p "Enter root password: " root_password
 echo
 read -s -p "Confirm root password: " root_password_confirm
 echo
 if [[ "$root_password" != "$root_password_confirm" ]]; then
-    echo "Passwords do not match. Exiting."
+    print_color "$RED" "Passwords do not match. Exiting."
     exit 1
 fi
 
@@ -273,11 +302,12 @@ echo
 read -s -p "Confirm password for $username: " user_password_confirm
 echo
 if [[ "$user_password" != "$user_password_confirm" ]]; then
-    echo "Passwords do not match. Exiting."
+    print_color "$RED" "Passwords do not match. Exiting."
     exit 1
 fi
 
 # Chroot and configure the system
+print_color "$CYAN" "Configuring the system..."
 arch-chroot /mnt /bin/bash << EOF
 # Set time zone
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
@@ -322,11 +352,28 @@ systemctl enable NetworkManager
 exit
 EOF
 
+# Ask if the user wants to copy the gaming setup script
+print_color "$YELLOW" "Would you like to copy the arch-gaming-setup.sh script to the new user's home directory?"
+read -p "This will allow you to easily run it after rebooting. (y/n): " copy_script
+
+if [[ $copy_script == "y" || $copy_script == "Y" ]]; then
+    # Copy the script to the new user's home directory
+    cp /root/arch-gaming-setup/arch-gaming-setup.sh /mnt/home/$username/
+    # Set the correct ownership
+    arch-chroot /mnt chown $username:$username /home/$username/arch-gaming-setup.sh
+    # Make the script executable
+    arch-chroot /mnt chmod +x /home/$username/arch-gaming-setup.sh
+    print_color "$GREEN" "The arch-gaming-setup.sh script has been copied to /home/$username/"
+    print_color "$BLUE" "After rebooting, you can run it with: ./arch-gaming-setup.sh"
+else
+    print_color "$BLUE" "The script was not copied. You can still clone the repository and run it after rebooting if you want."
+fi
+
 # Clear sensitive variables
 unset root_password root_password_confirm user_password user_password_confirm
 
 # Unmount and reboot
 umount -R /mnt
-echo "Installation completed. System will now reboot."
+print_color "$GREEN" "Installation completed. System will now reboot."
 read -p "Press Enter to reboot..."
 reboot
