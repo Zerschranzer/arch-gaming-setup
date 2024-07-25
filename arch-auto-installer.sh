3a81e7fb16186700b600d8945f88e7a9aead05c9
Zerschranzer <zerschranzer@googlemail.com>
2024-07-25 20:26:46 +0100

    Update arch-auto-installer.sh

--- arch-auto-installer.sh
+++ arch-auto-installer.sh
@@ -409,7 +409,8 @@ pacman -S --noconfirm sudo
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 # Install bootloader (GRUB for UEFI)
-pacman -S --noconfirm grub efibootmgr
+pacman -S --noconfirm grub efibootmgr os-prober
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
